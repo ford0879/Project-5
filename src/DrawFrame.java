b9d4614bccb68616617f0e20a4329b885d81eeaa
Derek <Derek@10.204.114.82>
2019-04-23 16:06:31 -0500

    Created Show Station, Calculate HD, and Add Station buttons. Still need to put in proper position and add functionality

--- src/DrawFrame.java
+++ src/DrawFrame.java
@@ -1,4 +1,6 @@
 import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -20,6 +22,11 @@ public class DrawFrame extends JFrame
 	private static final int FRAME_WIDTH = 600;
 	private static final int FRAME_HEIGHT = 750;
 	
+	/**
+	 * panel to hold all panels
+	 */
+	DrawPanel mainPanel = new DrawPanel();
+	
 	/** panel to hold all panels */
     JPanel panel0 = new JPanel(new GridLayout(2, 2));
 	/** panel for Hamm Slider, Show Station, and Station box */
@@ -30,6 +37,19 @@ public class DrawFrame extends JFrame
     JPanel panel3 = new JPanel(new GridLayout(3, 0));
     /** panel for Free Zone */
     JPanel panel4 = new JPanel();
+    
+    /**
+     * Button to compare given station with other stations of specific hamming distance
+     */
+    JButton showStation = new JButton("Show Station");
+    /**
+     * Button to compare given station with all stations
+     */
+    JButton calculateHD = new JButton("Calculate HD");
+    /**
+     * Button to add a station to list of stations
+     */
+    JButton addStation = new JButton("Add Station");
 	
 	
 	/**
@@ -42,6 +62,17 @@ public class DrawFrame extends JFrame
 
 	    this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 	    this.setLayout(new GridLayout(2, 0));
+
+        panel1.add(showStation);
+        panel2.add(calculateHD);
+        panel3.add(addStation);
+        
+        mainPanel.add(panel1);
+        mainPanel.add(panel2);
+        mainPanel.add(panel3);
+        
+        this.add(mainPanel);
+        
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
 	}
